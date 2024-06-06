de3caad0df9440883ecbf09e3bf7d4c44c168820
Kukurekovic <kukurekovicmarko@gmail.com>
2024-06-06 23:06:42 +0200

    11. commit

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,57 +1,66 @@
 import Add from "@/components/Add";
 import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductsImages from "@/components/ProductsImages";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  console.log(params.slug);
+  const wixClient = await wixClientServer();
+
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+  if (!products.items[0]) {
+    return notFound;
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductsImages />
+        <ProductsImages items={product.media?.items} />
       </div>
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-          consequuntur atque, architecto dolorum unde dicta fuga ratione et
-          consectetur quia? Tempore consequuntur atque, architecto dolorum unde
-          dicta fuga ratione et consectetur quia?
-        </p>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">$59</h3>
-          <h2 className="font-medium text-2xl">$49</h2>
-        </div>
+
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">${product.price?.price}</h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ${product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              ${product.price?.discountedPrice}
+            </h2>
+          </div>
+        )}
+
         <div className="h-[2px] bg-gray-100" />
-        <CustomizeProducts />
+        {product.variants && product.productOptions && (
+          <CustomizeProducts
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        )}
         <Add />
         <div className="h-[2px] bg-gray-100" />
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ex illum
-            nulla mollitia laborum error laudantium at dignissimos distinctio
-            minima! Explicabo accusamus voluptatum expedita qui.
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ex illum
-            nulla mollitia laborum error laudantium at dignissimos distinctio
-            minima! Explicabo accusamus voluptatum expedita qui.
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ex illum
-            nulla mollitia laborum error laudantium at dignissimos distinctio
-            minima! Explicabo accusamus voluptatum expedita qui.
-          </p>
-        </div>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className="text-sm" key={section.title}>
+            <h4 className="font-medium mb-4">{section.title}</h4>
+            <p>{section.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
