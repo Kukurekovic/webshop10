cc153648ab287d16c3e385cc4690b0aed795aa4c
Kukurekovic <kukurekovicmarko@gmail.com>
2024-06-08 22:26:26 +0200

    13. commit

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useWixClient } from "@/hooks/useWixClient";
 import React, { useState } from "react";
 
 const Add = ({
@@ -23,6 +24,21 @@ const Add = ({
     if (type === "i" && quantity < stockNumber) {
       setQuantity((prev) => prev + 1);
     }
+  };
+  const wixClient = useWixClient();
+  const addItem = async () => {
+    const response = await wixClient.currentCart.addToCurrentCart({
+      lineItems: [
+        {
+          catalogReference: {
+            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
+            catalogItemId: productId,
+            ...(variantId && { options: { variantId } }),
+          },
+          quantity: quantity,
+        },
+      ],
+    });
   };
 
   return (
@@ -56,7 +72,10 @@ const Add = ({
           )}
         </div>
 
-        <button className="w-36 text-sm rounded-3xl ring-1 ring-redT text-redT py-2 px-4 hover:bg-redT hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+        <button
+          onClick={addItem}
+          className="w-36 text-sm rounded-3xl ring-1 ring-redT text-redT py-2 px-4 hover:bg-redT hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none"
+        >
           Add to Cart
         </button>
       </div>
--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,12 +1,26 @@
 "use client";
+import { useCartStore } from "@/hooks/useCartStore";
+import { useWixClient } from "@/hooks/useWixClient";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 
 const CartModal = () => {
-  const cartItems = true;
+  //TEMPORARY
+  //const cartItems = true;
+
+  const wixClient = useWixClient();
+
+  const { cart, getCart } = useCartStore();
+
+  useEffect(() => {
+    getCart(wixClient);
+  }, [wixClient, getCart]);
+
+  console.log(cart);
+
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
-      {!cartItems ? (
+      {!cart.lineItems ? (
         <div>Cart is Empty</div>
       ) : (
         <>
@@ -14,64 +28,42 @@ const CartModal = () => {
           {/* LIST */}
           <div className="flex flex-col gap-8">
             {/* ITEM */}
-            <div className="flex gap-4">
-              <Image
-                src="https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&w=400"
-                alt=""
-                width={72}
-                height={96}
-                className="object-cover rounded-md"
-              />
+            {cart.lineItems.map((item) => (
+              <div className="flex gap-4" key={item._id}>
+                <Image
+                  src="https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&w=400"
+                  alt=""
+                  width={72}
+                  height={96}
+                  className="object-cover rounded-md"
+                />
 
-              <div className="flex flex-col justify-between w-full">
-                {/* TOP */}
-                <div>
-                  {/* TITLE */}
-                  <div className="flex items-center justify-between gap-8">
-                    <h3 className="font-semibold">Product Name</h3>
-                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                <div className="flex flex-col justify-between w-full">
+                  {/* TOP */}
+                  <div>
+                    {/* TITLE */}
+                    <div className="flex items-center justify-between gap-8">
+                      <h3 className="font-semibold">
+                        {item.productName?.original}
+                      </h3>
+                      <div className="p-1 bg-gray-50 rounded-sm">
+                        ${item.price?.amount}
+                      </div>
+                    </div>
+                    {/* DESC */}
+                    <div className="text-sm text-gray-500">
+                      {item.availability?.status}
+                    </div>
                   </div>
-                  {/* DESC */}
-                  <div className="text-sm text-gray-500">available</div>
-                </div>
 
-                {/* BOTTOM */}
-                <div className="flex justify-between text-sm">
-                  <span className="text-gray-500">Qty. 2</span>
-                  <span className="text-blue-500">Remove</span>
+                  {/* BOTTOM */}
+                  <div className="flex justify-between text-sm">
+                    <span className="text-gray-500">Qty. {item.quantity}</span>
+                    <span className="text-blue-500">Remove</span>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            {/* ITEM */}
-            <div className="flex gap-4">
-              <Image
-                src="https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&w=400"
-                alt=""
-                width={72}
-                height={96}
-                className="object-cover rounded-md"
-              />
-
-              <div className="flex flex-col justify-between w-full">
-                {/* TOP */}
-                <div>
-                  {/* TITLE */}
-                  <div className="flex items-center justify-between gap-8">
-                    <h3 className="font-semibold">Product Name</h3>
-                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
-                  </div>
-                  {/* DESC */}
-                  <div className="text-sm text-gray-500">available</div>
-                </div>
-
-                {/* BOTTOM */}
-                <div className="flex justify-between text-sm">
-                  <span className="text-gray-500">Qty. 2</span>
-                  <span className="text-blue-500">Remove</span>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
           {/* BOTTOM */}
           <div>
