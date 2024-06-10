a6e8c018e4d5afe0e0c3dd25d363b16ec6cba077
Kukurekovic <kukurekovicmarko@gmail.com>
2024-06-10 23:07:25 +0200

    14. commit

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCartStore } from "@/hooks/useCartStore";
 import { useWixClient } from "@/hooks/useWixClient";
 import React, { useState } from "react";
 
@@ -25,21 +26,10 @@ const Add = ({
       setQuantity((prev) => prev + 1);
     }
   };
+
   const wixClient = useWixClient();
-  const addItem = async () => {
-    const response = await wixClient.currentCart.addToCurrentCart({
-      lineItems: [
-        {
-          catalogReference: {
-            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
-            catalogItemId: productId,
-            ...(variantId && { options: { variantId } }),
-          },
-          quantity: quantity,
-        },
-      ],
-    });
-  };
+
+  const { addItem, isLoading } = useCartStore();
 
   return (
     <div className="flex flex-col gap-4">
@@ -73,8 +63,9 @@ const Add = ({
         </div>
 
         <button
-          onClick={addItem}
-          className="w-36 text-sm rounded-3xl ring-1 ring-redT text-redT py-2 px-4 hover:bg-redT hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none"
+          onClick={() => addItem(wixClient, productId, variantId, quantity)}
+          disabled={isLoading}
+          className="w-36 text-sm rounded-3xl ring-1 ring-redT text-redT py-2 px-4 hover:bg-redT hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
         >
           Add to Cart
         </button>
--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,22 +1,15 @@
 "use client";
 import { useCartStore } from "@/hooks/useCartStore";
-import { useWixClient } from "@/hooks/useWixClient";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React from "react";
+import { media as wixMedia } from "@wix/sdk";
+import { useWixClient } from "@/hooks/useWixClient";
 
 const CartModal = () => {
   //TEMPORARY
   //const cartItems = true;
-
   const wixClient = useWixClient();
-
-  const { cart, getCart } = useCartStore();
-
-  useEffect(() => {
-    getCart(wixClient);
-  }, [wixClient, getCart]);
-
-  console.log(cart);
+  const { cart, isLoading, removeItem } = useCartStore();
 
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
@@ -30,13 +23,20 @@ const CartModal = () => {
             {/* ITEM */}
             {cart.lineItems.map((item) => (
               <div className="flex gap-4" key={item._id}>
-                <Image
-                  src="https://images.pexels.com/photos/534172/pexels-photo-534172.jpeg?auto=compress&cs=tinysrgb&w=400"
-                  alt=""
-                  width={72}
-                  height={96}
-                  className="object-cover rounded-md"
-                />
+                {item.image && (
+                  <Image
+                    src={wixMedia.getScaledToFillImageUrl(
+                      item.image,
+                      72,
+                      96,
+                      {}
+                    )}
+                    alt=""
+                    width={72}
+                    height={96}
+                    className="object-cover rounded-md"
+                  />
+                )}
 
                 <div className="flex flex-col justify-between w-full">
                   {/* TOP */}
@@ -46,7 +46,12 @@ const CartModal = () => {
                       <h3 className="font-semibold">
                         {item.productName?.original}
                       </h3>
-                      <div className="p-1 bg-gray-50 rounded-sm">
+                      <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
+                        {item.quantity && item.quantity > 1 && (
+                          <div className="text-xs text-green-500">
+                            {item.quantity} x{" "}
+                          </div>
+                        )}
                         ${item.price?.amount}
                       </div>
                     </div>
@@ -59,7 +64,13 @@ const CartModal = () => {
                   {/* BOTTOM */}
                   <div className="flex justify-between text-sm">
                     <span className="text-gray-500">Qty. {item.quantity}</span>
-                    <span className="text-blue-500">Remove</span>
+                    <span
+                      className="text-blue-500"
+                      style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
+                      onClick={() => removeItem(wixClient, item._id!)}
+                    >
+                      Remove
+                    </span>
                   </div>
                 </div>
               </div>
@@ -69,7 +80,7 @@ const CartModal = () => {
           <div>
             <div className="flex items-center justify-between font-semibold">
               <span>Subtotal</span>
-              <span>$49</span>
+              <span>${cart.subtotal.amount}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
               Shipping and taxes calculated at checkout.
@@ -78,7 +89,10 @@ const CartModal = () => {
               <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
                 View Cart
               </button>
-              <button className="rounded-md py-3 px-4 bg-black text-white">
+              <button
+                className="rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
+                disabled={isLoading}
+              >
                 Checkout
               </button>
             </div>
